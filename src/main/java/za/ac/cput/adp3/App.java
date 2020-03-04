ac53431d49231374c00c7b082ca8ecc8135c5953
CameronJulian <60965234+CameronJulian@users.noreply.github.com>
2020-03-04 12:33:30 +0200

    Add files via upload

---
+++ src/main/java/za/ac/cput/adp3/App.java
@@ -0,0 +1,10 @@
+package za.ac.cput.adp3;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to my first maven project." );
+    }
+}
